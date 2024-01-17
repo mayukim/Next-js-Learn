c9a6a8a5339a21f64853cb8e14584a2081088067
mayukim <111472634+mayukim@users.noreply.github.com>
2024-01-17 23:38:18 +0900

    Update

--- forum/app/list/listItem.js
+++ forum/app/list/listItem.js
@@ -1,16 +1,46 @@
 "use client";
 
-export default function ListItem() {
+import Link from "next/link";
+
+export default function ListItem({ result }) {
     return (
         <div>
-            {result.map((a, i) => (
+            {props.result.map((a, i) => (
                 <div className="list-item" key={i}>
                     <Link prefetch={false} href={"/detail/" + result[i]._id}>
                         <h4>{result[i].title}</h4>
                     </Link>
-                    <Link href={"/edit/" + result[i]._id} className="list-btn">
-                        ✏️
-                    </Link>
+                    <Link href={"/edit/" + result[i]._id}>✏️</Link>
+                    <span
+                        onClick={(e) => {
+                            fetch("/api/post/delete", {
+                                method: "DELETE",
+                                body: result[i]._id,
+                            })
+                                .then((r) => {
+                                    if (r.status == 200) {
+                                        return r.json();
+                                    } else {
+                                        //서버가 에러코드전송시 실행할코드
+                                    }
+                                })
+                                .then((result) => {
+                                    //성공시 실행할코드
+                                    e.target.parentElement.style.opacity = 0;
+                                    setTimeout(() => {
+                                        e.target.parentElement.style.display =
+                                            "none";
+                                    }, 1000);
+                                })
+                                .catch((error) => {
+                                    //인터넷문제 등으로 실패시 실행할코드
+                                    console.log(error);
+                                });
+                            // fetch('/api/abc/kim')
+                        }}
+                    >
+                        🗑️
+                    </span>
                     <DetailLink />
                     <p>{result[i].content}</p>
                 </div>
--- forum/app/list/page.js
+++ forum/app/list/page.js
@@ -4,13 +4,15 @@ import { connectDB } from "@/util/database";
 import Link from "next/link";
 import ListItem from "./listItem";
 
+export const dynamic = "force-dynamic";
+
 export default async function List() {
     const db = (await connectDB).db("forum");
     let result = await db.collection("post").find().toArray();
 
     return (
         <div className="list-bg">
-            <ListItem />
+            <ListItem result={result} />
         </div>
     );
 }
